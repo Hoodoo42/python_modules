de2b1a6aaffd997979c1eebc6b6365bde70ffada
Hoodoo42 <create@hoodooverse.ca>
2022-09-24 11:12:20 -0600

    ran antigravity from library

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import antigravity
+# one way to locally import the log.py into app.py is specifying the specific function(s) you want to use
 from helpers.log import logging_logs
 
+
+# and then call imported functions
 logging_logs('Day One')
 logging_logs('Day Two')
 logging_logs('Day Three')
